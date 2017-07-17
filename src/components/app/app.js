f675f0e16bf8a42dd78df4909421fa1e15e27577
Mustafa <mjebbar17@gmail.com>
2017-07-17 11:35:56 +0100

    first commit

---
+++ src/components/app/app.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import './app.sass';
+
+class App extends Component {
+  render() {
+
+    return (
+      <div className="app">
+          HELLO WORLD!
+      </div>
+    );
+  }
+}
+
+export default App;
